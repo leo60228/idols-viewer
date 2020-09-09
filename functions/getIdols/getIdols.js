bfbefb072dff078e97495add8409d3b3edbdc94e
leo60228 <iakornfeld@gmail.com>
2020-09-09 19:59:05 -0400

    retry

--- functions/getIdols/getIdols.js
+++ functions/getIdols/getIdols.js
@@ -1,18 +1,38 @@
 const fetch = require('node-fetch')
+const AbortController = require('abort-controller')
+const promiseRetry = require('promise-retry')
+
+function fetchJSON(url) {
+  return promiseRetry(async (retry) => {
+    const controller = new AbortController()
+    const timeout = setTimeout(() => controller.abort(), 1000)
+
+    try {
+      const res = await fetch(url, { signal: controller.signal })
+      const body = await res.json()
+      return body
+    } catch (e) {
+      if (e.name === 'AbortError') {
+        retry()
+      } else {
+        throw e
+      }
+    } finally {
+      clearTimeout(timeout)
+    }
+  }, {
+    factor: 1,
+    minTimeout: 500,
+    randomize: true
+  })
+}
+
 exports.handler = async function(event, context) {
   try {
-    let response = await fetch('https://www.blaseball.com/api/getIdols')
-    if (!response.ok) {
-      return { statusCode: response.status, body: response.statusText }
-    }
-    const data = await response.json()
+    const data = await fetchJSON('https://www.blaseball.com/api/getIdols')
     const playerIds = data.map(x => x.playerId)
 
-    response = await fetch(`https://www.blaseball.com/database/players?ids=${playerIds.join(',')}`)
-    if (!response.ok) {
-      return { statusCode: response.status, body: response.statusText }
-    }
-    const playerData = await response.json()
+    const playerData = await fetchJSON(`https://www.blaseball.com/database/players?ids=${playerIds.join(',')}`)
 
     const idols = data.map(x => Object.assign(x, { player: playerData.find(y => x.playerId == y.id) }))
 
